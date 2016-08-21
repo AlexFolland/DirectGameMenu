29de8f7e30d6a9bef4620df31ea7faf59da43b16
AlexFolland <lexlexlex@gmail.com>
2016-08-21 08:36:19 -0700

    set UI to visible if not

--- DirectGameMenu.lua
+++ DirectGameMenu.lua
@@ -1,9 +1,13 @@
 ToggleGameMenu = function()
-    if ( GameMenuFrame:IsShown() ) then
-        PlaySound("igMainMenuQuit");
-        HideUIPanel(GameMenuFrame);
+    if not UIParent:IsShown() then
+        UIParent:Show()
+        SetUIVisibility(true)
+    end
+    if GameMenuFrame:IsShown() then
+        PlaySound("igMainMenuQuit")
+        HideUIPanel(GameMenuFrame)
     else
-        PlaySound("igMainMenuOpen");
-        ShowUIPanel(GameMenuFrame);
+        PlaySound("igMainMenuOpen")
+        ShowUIPanel(GameMenuFrame)
     end
 end
